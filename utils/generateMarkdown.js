8208aacba41dc3c55df44b3e687b057bc07f0bb4
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-07-22 20:17:48 -0400

    added index and generate markdown

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,31 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  1.[Description](#description)
+  2.[Installation](#installation)
+  3.[Usage](#usage)
+  4.[License](#license)
+  5.[Contributing](#contributing)
+  6.[Test](#tests)
+  7.[Questions](#questions)
+  ### Description
+  *${data.description}
+  ### Installation
+  *${data.installation}
+  ### Usage
+  *${data.usage}
+  ### License
+  * ${data.license}
+  ### Contributing
+  * ${data.contributing}
+  ### Tests
+  * ${data.tests}
+  #### Questions
+  * [${data.github}](https://github.com/${data.github})
+  * ${data.email}
+
+`;
+}
+
+module.exports = generateMarkdown;
